bea2bb3e44bce41218c5bf679832d2b0445dc774
Bharat <bharatappguru@gmail.com>
2015-02-20 11:50:31 +0530

    Working With Unity UI 4.6

---
+++ Assets/scripts/UIMangerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIMangerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Play Button Click Event
+	public void OnPlayButtonClick(){
+		Debug.Log("Play Button clicked");
+	}
+
+	//Setting  Button Click
+
+	public void OnSettingsButtonClick(Canvas can){
+		Debug.Log("Settings Button Click");
+	}
+
+	// exit button click event
+	public void OnExitbuttonClick(){
+		Debug.Log("Exit Button click");
+	}
+}
